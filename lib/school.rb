63e779704673b42a267156114d9f0dca5b50acd2
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 20:00:21 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :roster
+  
+  #NEW = @roster
+  
+  def initialize(name)
+    
+    @roster = { }                         #creates hash
+  end
+  
+  
+  def add_student(student_name, grade)
+    @roster[grade] ||= [ ]
+    @roster[grade] << student_name        #assigns student names to grade, key/value pairs in hash
+  end
+  
+  def grade(num)
+    @roster[num]                          #returns student names depending on grade/num received
+    
+  end
+  
+  def sort
+    sorted_hash = { }
+    @roster.each do |grade, names|
+      sorted_hash[grade] = @roster[grade].sort
+
+    end
+    sorted_hash
+  end
+  
+end
+
