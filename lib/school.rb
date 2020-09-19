fe02fc07d04d23194df59a61c0c8b35de1ce9065
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-19 17:51:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,10 +11,9 @@ class School
   
   def add_student(student_name, grade)
     @roster[grade] ||= [ ]
-    @roster[student_name] << student_name
+    @roster[grade] << student_name
   end
   
-  def grade 
-  
+
 end
 
